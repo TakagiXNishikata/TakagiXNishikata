54e471c3dbc6494ee7e8ad7f1e419fe9ec6ffb64
Raffi Ananta <raffiesnew@gmail.com>
2024-11-01 22:18:06 +0700

    fix:test 1

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,18 +1,18 @@
 export default function Navigation({ data, onPress, spyId, classNameId }) {
     const classActive = spyId === classNameId ? "active" : "";
-    
-    return (
-        <li data-to-scrollspy-id={data?.scrollspy_id}>
-            <a 
-                onClick={(e) => onPress(e)}
-                href={data?.href}
-                className={`group flex items-center py-3 ${classActive}`}
-            >   
-                <span className="nav-indicator mr-4 h-px w-8 bg-shadow-gray transition-all group-hover:w-16 group-hover:bg-bone-white group-focus-visible::w-16 group-focus-visible:bg-bone-white motion-reduce:transition-none"></span>
-                <span className="nav-text text-xs font-bold uppercase tracking-widest text-shadow-gray group-hover:text-bone-white group-focus-visible:text-bone-white">
-                {data?.link_text}
-                </span>
-            </a>
-        </li>
-    )
+
+return (
+    <li data-to-scrollspy-id={data?.scrollspy_id}>
+    <a
+        onClick={(e) => onPress(e)}
+        href={data?.href}
+        className={`group flex items-center py-3 ${classActive}`}
+    >
+        <span className="nav-indicator mr-4 h-px w-8 bg-slate-400 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"></span>
+        <span className="nav-text text-xs font-bold uppercase tracking-widest text-slate-400 group-hover:text-slate-200 group-focus-visible:text-slate-200">
+        {data?.link_text}
+        </span>
+    </a>
+    </li>
+);
 }
